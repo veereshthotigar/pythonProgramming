eab42a5a730adedb1310c25a07ab6195f72aa834
Veeresha Thotigar <veeresh.thotygar@gmail.com>
2020-02-16 21:25:46 +0000

    OOPS concept

---
+++ inheritance.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+class Parent:        # define parent class
+   parentAttr = 100
+   def __init__(self):
+      print ("Calling parent constructor")
+
+   def parentMethod(self):
+      print ('Calling parent method')
+
+   def setAttr(self, attr):
+      Parent.parentAttr = attr
+
+   def getAttr(self):
+      print ("Parent attribute :", Parent.parentAttr)
+
+class Child(Parent): # define child class
+   def __init__(self):
+      print ("Calling child constructor")
+
+   def childMethod(self):
+      print ('Calling child method')
--- main.py
+++ main.py
@@ -1,3 +1,47 @@
-import addition_of_numbers as a
+#import addition_of_numbers as a
+#print(a.add(2,3))
+'''
+import employee as e
 
-print(a.add(2,3))
+emp1 = e.Employee("Zara",20)
+emp2 = e.Employee("John",22)
+
+emp1.displayEmployee()
+emp2.displayEmployee()
+
+emp1.displayCount()
+print ("Total Employee %d" % e.Employee.empCount)
+'''
+
+
+'''
+import inheritance as inh
+
+c = inh.Child()          # instance of child
+c.childMethod()      # child calls its method
+c.parentMethod()     # calls parent's method
+c.setAttr(200)       # again call parent's method
+c.getAttr()          # again call parent's method
+'''
+
+'''
+import operatorOverloading as op
+
+v1 = op.Vector(2,10)
+v2 = op.Vector(5,-2)
+print (v1 + v2)
+'''
+
+'''
+import privateClassMember as priv
+
+counter = priv.JustCounter()
+counter.count()
+counter.count()
+#print (counter.__secretCount)
+print (counter._JustCounter__secretCount)
+'''
+
+
+
+
